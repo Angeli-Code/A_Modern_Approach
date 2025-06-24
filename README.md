5fc407dc738438ad9d8371e9617385fcde57a28c
Angelo Laberinto <ashhlabb@gmail.com>
2025-06-23 21:15:39 -0700

    Init Commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# C Programming: A Modern Approach
+
+I will use this directory to track my "official" progress through the book.
+
+## Author
+
+Angelo Laberinto        Summer 2025
+
---
+++ nike.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/*
+ * Print this to the console
+ *
+ *	        *
+ *	       *
+ *	      *
+ *	 *   *
+ *	  * *
+ *	   *
+ *
+ *     Just Do It
+ */
+
+int main(void)
+{
+	printf("       *\n");
+	printf("      *\n");
+	printf("     *\n");
+	printf("*   *\n");
+	printf(" * *\n");
+	printf("  *\n");
+
+	return 0;
+}
+
